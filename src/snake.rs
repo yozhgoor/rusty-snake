60a7607a20ee93168f4faedfe1f9aeb8de2a06f4
yozhgoor <yozhgoor@outlook.com>
2021-01-29 20:05:48 +0100

    Implement the new method in Snake

--- src/snake.rs
+++ src/snake.rs
@@ -7,3 +7,15 @@ pub struct Snake {
     direction: Direction,
     digesting: bool,
 }
+
+impl Snake {
+    pub fn new(start: Point, length: u16, direction: Direction) -> Self {
+        let opposite = direction.opposite();
+        let body: Vec<Point> = (0..length)
+            .into_iter()
+            .map(|i| start.transform(opposite, i))
+            .collect();
+
+        Self { body, direction, digesting: false }
+    }
+}
