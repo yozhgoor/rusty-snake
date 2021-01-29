de4180d947ed99d0a07ea2ac47f44fee6c5f37f8
yozhgoor <yozhgoor@outlook.com>
2021-01-29 21:44:31 +0100

    Define the Game struct

--- src/games.rs
+++ src/games.rs
@@ -1,3 +1,15 @@
 const MAX_INTERVAL: u16 = 700;
 const MIN_INTERVAL: u16 = 200;
 const MAX_SPEED: u16 = 20;
+
+#[derive(Debug)]
+pub struct Game {
+    stdout: Stdout,
+    original_terminal_size: (u16, u16),
+    width: u16,
+    height: u16,
+    food: Option<Point>,
+    snake: Snake,
+    speed: u16,
+    score: u16,
+}
