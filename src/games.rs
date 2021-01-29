37b7d52f9b2a9a968b0cb7d88eb212433a2d9969
yozhgoor <yozhgoor@outlook.com>
2021-01-29 23:35:06 +0100

    Implement the draw_borders method in Game

--- src/games.rs
+++ src/games.rs
@@ -122,4 +122,33 @@ impl Game {
         self.draw_food();
         self.draw_snake();
     }
+    fn draw_borders(&mut self) {
+        self.stdout.execute(SetForeground(Color::DarkGrey)).unwrap();
+
+        for y in 0..self.height + 2 {
+            self.stdout
+                .execute(MoveTo(0, y)).unwrap()
+                .execute(Print("#")).unwrap()
+                .execute(MoveTo(self.width + 1, y)).unwrap()
+                .execute(Print("#")).unwrap();
+        }
+
+        for x in 0..self.width + 2 {
+            self.stdout
+                .execute(MoveTo(x, 0)).unwrap()
+                .execute(Print("#")).unwrap()
+                .execute(MoveTo(self.width + 1, y)).unwrap()
+                .execute(Print("#")).unwrap();
+        }
+
+        self.stdout
+            .execute(MoveTo(0, 0)).unwrap()
+            .execute(Print("#")).unwrap()
+            .execute(MoveTo(self.width + 1, self.height + 1)).unwrap()
+            .execute(Print("#")).unwrap()
+            .execute(MoveTo(self.width + 1, 0)).unwrap()
+            .execute(Print("#")).unwrap()
+            .execute(MoveTo(0, self.height + 1)).unwrap()
+            .execute(Print("#")).unwrap();
+    }
 }
