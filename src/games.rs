c671641ac0d75278805bc3f7db997ff5e6cf6052
yozhgoor <yozhgoor@outlook.com>
2021-01-29 22:34:56 +0100

    Implement the prepare_ui method in Game

--- src/games.rs
+++ src/games.rs
@@ -109,4 +109,11 @@ impl Game {
             }
         }
     }
+    pub fn prepare_ui(&mut self) {
+        enable_raw_mode().unwrap();
+        self.stdout
+            .execute(SetSize(self.width + 3, self.height + 3)).unwrap()
+            .execute(Clear(ClearType::All)).unwrap()
+            .execute(Hide).unwrap();
+    }
 }
