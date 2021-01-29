a6ecb48aa0408f3b4ee36f6ccf0440f2aa960b24
Yohan Boogaert <yozhgoor@outlook.com>
2021-01-29 21:29:33 +0100

    Snake (#2)
    * Create snake.rs
    
    * Define the Snake struct
    
    * Implement the new method in Snake
    
    * Implement the get_head_point method in Snake
    
    * Implement the get_body_points method in Snake
    
    * Implement the get_direction method in Snake
    
    * Implement the contains_points method in Snake
    
    * Implement the slither method in Snake
    
    * Implement the set_direction method in Snake
    
    * Implement the grow method in Snake
    
    * Implement the opposite method in Direction

--- src/direction.rs
+++ src/direction.rs
@@ -5,3 +5,14 @@ pub enum Direction {
     Down,
     Left,
 }
+
+impl Direction {
+    pub fn opposite(&self) -> Self {
+        match self {
+            Self::Up => Self::Down,
+            Self::Right => Self::Left,
+            Self::Down => Self::Up,
+            Self::Left => Self::Right,
+        }
+    }
+}
