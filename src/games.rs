efe09bb8fe589932a7d41e8b3b4114bfaf465e58
Yohan Boogaert <yozhgoor@outlook.com>
2021-01-29 22:35:42 +0100

    Game logic (#3)
    * Add crossterm and rand as dependencies
    
    * Create game.rs
    
    * Add the game constants
    
    * Define the Game struct
    
    * Implement the new method in Game
    
    * Use needed modules
    
    * Implement the run method in Game
    
    * Complete the run method
    
    * Implement the place_food in Game
    
    * Implement the prepare_ui method in Game

---
+++ src/games.rs
@@ -0,0 +1,119 @@
+use crate::snake::Snake;
+use crate::point::Point;
+use crate::direction::Direction;
+use std::io::Stdout;
+use std::time{Duration, Instant};
+use crossterm::terminal::size;
+use crate::command::Command;
+use rand::Rng;
+
+const MAX_INTERVAL: u16 = 700;
+const MIN_INTERVAL: u16 = 200;
+const MAX_SPEED: u16 = 20;
+
+#[derive(Debug)]
+pub struct Game {
+    stdout: Stdout,
+    original_terminal_size: (u16, u16),
+    width: u16,
+    height: u16,
+    food: Option<Point>,
+    snake: Snake,
+    speed: u16,
+    score: u16,
+}
+
+impl Game {
+    pub fn new(stdout: Stdout, width: u16, height: u16) -> Self {
+        let original_terminal_size: (u16, u16) = size.unwrap();
+        Self{
+            stdout,
+            original_terminal_size,
+            width,
+            height,
+            food: None,
+            Snake: Snake::new(
+                Point::new(width / 2, height / 2),
+                3,
+                match rand::thread_rng().gen_range(0, 4) {
+                    0 => Direction::Up,
+                    1 => Direction::Right,
+                    2 => Direction::Down,
+                    _ => Direction::Left,
+                },
+            ),
+            speed: 0,
+            score: 0,
+        }
+    }
+    pub fn run(&mut self) {
+        self.place_food();
+        self.prepare_ui();
+        self.render();
+
+        let mut done = false;
+        while !done {
+            let interval = self.calculate_interval();
+            let direction = self.snake.get_direction();
+            let now = Instant::now();
+
+            while now.elapsed() < interval {
+                if let Some(command) = self.get_command(interval - now.elapsed()) {
+                    match command {
+                        Command::Quit => {
+                            done = true;
+                            break;
+                        }
+                        Command::Turn(towards) => {
+                            if direction != towards && direction.opposite() =! towards {
+                                self.snake.set_direction(towards);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if self.has_collided_with_wall() || self.has_bitten_itself() {
+                done = true;
+            } else {
+                self.snake.slither();
+
+                if let Some(food_point) = self.food {
+                    if self.snake.get_head_point() == food_point {
+                        self.snake.grow();
+                        self.place_food();
+                        self.score += 1;
+
+                        if self.score % ((self.width * self.height) / MAX_SPEED) == 0 {
+                            self.speed += 1;
+                        }
+                    }
+                }
+
+                self.render();
+            }
+        }
+
+        self.restore_ui();
+
+        println!("Game Over! Your score is {}", self.score);
+    }
+    pub fn place_food(&mut self) {
+        loop {
+            let random_x = rand::thread_rng().gen_range(0, self.width);
+            let random_y = rand::thread_rng().gen_range(0, self.height);
+            let point = Point::new(random_x, random_y);
+            if !self.snake.contains_point(&point) {
+                self.food = Some(Point);
+                break;
+            }
+        }
+    }
+    pub fn prepare_ui(&mut self) {
+        enable_raw_mode().unwrap();
+        self.stdout
+            .execute(SetSize(self.width + 3, self.height + 3)).unwrap()
+            .execute(Clear(ClearType::All)).unwrap()
+            .execute(Hide).unwrap();
+    }
+}
