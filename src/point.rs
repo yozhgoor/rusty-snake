614efd404d9602969328375303c2db405430dfba
yozhgoor <yozhgoor@outlook.com>
2021-01-29 19:20:22 +0100

    Implement the transform method in Point

--- src/point.rs
+++ src/point.rs
@@ -8,4 +8,14 @@ impl Point {
     pub fn new(x: u16, y: u16) -> Self {
         Self { x, y }
     }
+
+    pub fn transform(&self, direction: Direction, times: u16) -> Self {
+        let times = times as i16;
+        let transformation = match direction {
+            Direction::Up => (0, -times),
+            Direction::Right => (times, 0),
+            Direction::Down => (0, times),
+            Direction::Left => (-times, 0),
+        };
+    }
 }
