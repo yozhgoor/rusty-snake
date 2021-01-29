ea5f76a8d2f1c72985c4bc72a037f1e3ba95f127
yozhgoor <yozhgoor@outlook.com>
2021-01-29 22:15:04 +0100

    Use needed modules

--- src/games.rs
+++ src/games.rs
@@ -1,3 +1,12 @@
+use crate::snake::Snake;
+use crate::point::Point;
+use crate::direction::Direction;
+use std::io::Stdout;
+use std::time{Duration, Instant};
+use crossterm::terminal::size;
+use crate::command::Command;
+use rand::Rng;
+
 const MAX_INTERVAL: u16 = 700;
 const MIN_INTERVAL: u16 = 200;
 const MAX_SPEED: u16 = 20;
